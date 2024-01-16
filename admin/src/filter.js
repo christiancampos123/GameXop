380b3ee24163b7e967bd0e5961db4f70867ea6d4
christiancampos123 <christiancampos1996@gmail.com>
2024-01-16 12:44:41 +0100

    end panel

--- admin/src/filter.js
+++
@@ -1,8 +0,0 @@
-export default (() => {
-    const filterButton = document.querySelector(".filter-button");
-
-    filterButton?.addEventListener("click", () => {
-        document.dispatchEvent(new CustomEvent('showFilterModal', {
-        }));
-    });
-})();
