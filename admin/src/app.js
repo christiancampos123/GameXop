a4a35afb88db77eaa166b21b1041d6b22ce2df11
christiancampos123 <christiancampos1996@gmail.com>
2024-01-16 11:10:16 +0100

    first commit

---
+++ admin/src/app.js
@@ -0,0 +1,9 @@
+import './formButtons.js';
+import './image.js';
+import './modalDestroy.js';
+import './formValidation.js';
+import './title.js';
+import './menuHamburguer.js';
+import './filterModal.js';
+import './tableRecords.js';
+import './form.js'
