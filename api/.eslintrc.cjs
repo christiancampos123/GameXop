91b6d96620d96b0a8750deda62a0c676ea7ada19
christian.campos <christian.campos@192-168-1-187>
2024-01-17 15:37:39 +0100

    yes

---
+++ api/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: './node_modules/standard/eslintrc.json',
+  env: {
+    browser: true, // Esto permite variables globales del navegador como `localStorage` y `sessionStorage`
+    es6: true // Para soportar características de ES6+
+  },
+  globals: {
+    customElements: 'readonly' // Esto le dice a ESLint que `customElements` es una variable global de solo lectura
+  },
+  rules: {
+    // Aquí puedes añadir o sobrescribir reglas específicas
+  }
+}
