1d53f294096d889b952bdd8800dd9e3c55c60fe7
christian.campos <christian.campos@192-168-1-187>
2024-01-22 15:46:00 +0100

    chabges models

---
+++ api/src/models/locale.js
@@ -0,0 +1,68 @@
+module.exports = function (sequelize, DataTypes) {
+  const Locale = sequelize.define('Locale', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    languageAlias: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entity: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    entityId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    value: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      get() {
+        return this.getDataValue('createdAt')
+          ? this.getDataValue('createdAt').toISOString().split('T')[0]
+          : null
+      }
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      get() {
+        return this.getDataValue('updatedAt')
+          ? this.getDataValue('updatedAt').toISOString().split('T')[0]
+          : null
+      }
+    }
+  },
+   {
+    sequelize,
+    tableName: 'locales',
+    timestamps: true,
+    paranoid: true,
+    indexes: [
+      {
+        name: 'PRIMARY',
+        unique: true,
+        using: 'BTREE',
+        fields: [
+          { name: 'id' }
+        ]
+      }
+    ]
+  })
+
+  Locale.associate = function (models) {
+
+  }
+
+  return Locale
+}
